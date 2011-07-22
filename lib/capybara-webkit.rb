a4538b620f3637b62182b0f52eff978238bdb784
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-07-22 17:29:35 -0400

    Register :webkit_debug driver

--- lib/capybara-webkit.rb
+++ lib/capybara-webkit.rb
@@ -5,3 +5,7 @@ Capybara.register_driver :webkit do |app|
   Capybara::Driver::Webkit.new(app)
 end
 
+Capybara.register_driver :webkit_debug do |app|
+  browser = Capybara::Driver::Webkit::Browser.new(:socket_class => Capybara::Driver::Webkit::SocketDebugger)
+  Capybara::Driver::Webkit.new(app, :browser => browser)
+end
