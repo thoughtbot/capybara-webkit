dc49e5f87587a8f735beebca26ec5800c92fb88c
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-08-15 18:17:14 +0200

    Forward webkit_server's stdout to Ruby's stdout.
    This allows console.log() messages to be visible, and prevents
    large numbers of console.log() calls from filling up webkit_server's pipe
    which would block the process.

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'stringio'
 require 'capybara/driver/webkit/browser'
 
 describe Capybara::Driver::Webkit::Browser do
@@ -21,6 +22,14 @@ describe Capybara::Driver::Webkit::Browser do
       new_browser = Capybara::Driver::Webkit::Browser.new
       new_browser.server_port.should_not == browser.server_port
     end
+  end
+  
+  it 'forwards stdout to the given IO object' do
+    io = StringIO.new
+    new_browser = Capybara::Driver::Webkit::Browser.new(:stdout => io)
+    new_browser.execute_script('console.log("hello world")')
+    sleep(0.5)
+    io.string.should == "hello world\n"
   end
 
 end
