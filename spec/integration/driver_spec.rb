37f5c5509352baff42fd01e3314c56215466691e
Matthew Mongeau <halogenandtoast@gmail.com>
2011-08-19 12:03:30 -0400

    Frames are supported.

--- spec/integration/driver_spec.rb
+++ spec/integration/driver_spec.rb
@@ -9,12 +9,12 @@ describe Capybara::Driver::Webkit do
   it_should_behave_like "driver"
   it_should_behave_like "driver with javascript support"
   it_should_behave_like "driver with cookies support"
+  it_should_behave_like "driver with header support"
+  it_should_behave_like "driver with status code support"
+  it_should_behave_like "driver with frame support"
 
   it "returns the rack server port" do
     @driver.server_port.should  eq(@driver.instance_variable_get(:@rack_server).port)
   end
 
-  it_should_behave_like "driver with header support"
-  it_should_behave_like "driver with status code support"
-  # it_should_behave_like "driver with frame support"
 end
