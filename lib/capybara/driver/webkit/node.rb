d69bf31cbe80fcf506a76993fa810e5aeff6259e
Niklas Baumstark <niklas.baumstark@gmail.com>
2011-09-13 16:59:46 +0200

    added method "submit" to Node

--- lib/capybara/driver/webkit/node.rb
+++ lib/capybara/driver/webkit/node.rb
@@ -74,6 +74,10 @@ class Capybara::Driver::Webkit
       invoke "path"
     end
 
+    def submit(opts)
+      invoke "submit"
+    end
+
     def trigger(event)
       invoke "trigger", event
     end
