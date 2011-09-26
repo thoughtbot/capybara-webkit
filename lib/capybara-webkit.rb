eeeddbd072410fa8c15d7e6afe640c1be1d47dda
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-09-26 11:33:25 -0300

    Follow the expected gem name/require path

--- lib/capybara-webkit.rb
+++ lib/capybara-webkit.rb
@@ -1,11 +1,1 @@
-require "capybara"
-require "capybara/driver/webkit"
-
-Capybara.register_driver :webkit do |app|
-  Capybara::Driver::Webkit.new(app)
-end
-
-Capybara.register_driver :webkit_debug do |app|
-  browser = Capybara::Driver::Webkit::Browser.new(:socket_class => Capybara::Driver::Webkit::SocketDebugger)
-  Capybara::Driver::Webkit.new(app, :browser => browser)
-end
+require "capybara/webkit"
