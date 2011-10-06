f76bd7020fdca450f8bbcf3473799733d655ca83
Niklas Baumstark <niklas.baumstark@gmail.com>
2011-10-06 15:07:45 +0200

    adapt wording of CookieJar specs (#179)

--- spec/cookie_jar_spec.rb
+++ spec/cookie_jar_spec.rb
@@ -16,17 +16,17 @@ describe Capybara::Driver::Webkit::CookieJar do
   subject { Capybara::Driver::Webkit::CookieJar.new(browser) }
 
   describe "#[]" do
-    it "should return the right cookie value for every given domain/path" do
+    it "returns the right cookie value for every given domain/path" do
       subject["cookie1", "example.org"].should == "1"
       subject["cookie1", "www.facebook.com"].should == "3"
       subject["cookie2", "sub1.example.org"].should == "4"
     end
 
-    it "should not return cookie from other domain" do
+    it "does not return a cookie from other domain" do
       subject["cookie2", "www.example.org"].should == nil
     end
 
-    it "should handle path precedence correctly" do
+    it "respects path precedence rules" do
       subject["cookie1", "www.example.org"].should == "1"
       subject["cookie1", "www.example.org", "/dir1/123"].should == "2"
     end
