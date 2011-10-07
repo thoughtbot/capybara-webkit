cf1de0138eb00ea555b4096ca202e0f5c8a76dba
Moreno Carullo <moreno.carullo@pulc.it>
2011-10-07 08:46:47 +0200

    Cleaner handling of windows/unix difference (which command)

--- lib/capybara_webkit_builder.rb
+++ lib/capybara_webkit_builder.rb
@@ -3,24 +3,23 @@ require "fileutils"
 module CapybaraWebkitBuilder
   extend self
 
+  def has_binary?(binary)
+    case RUBY_PLATFORM
+    when /mingw32/
+      system("#{binary} --version")
+    else
+      system("which #{make}")
+    end
+  end
+  
   def make_bin
     make_binaries = ['gmake', 'make']
-	case RUBY_PLATFORM
-	when /mingw32/
-	  make_binaries.last
-	else	  
-      make_binaries.detect { |make| system("which #{make}") }
-	end    
+    make_binaries.detect { |make| has_binary?(make) }
   end
 
   def qmake_bin
     qmake_binaries = ['qmake', 'qmake-qt4']
-    case RUBY_PLATFORM
-	when /mingw32/
-	  qmake_binaries.first
-	else
-	  qmake_binaries.detect { |qmake| system("which #{qmake}") }
-	end
+    qmake_binaries.detect { |qmake| has_binary?(qmake) }
   end
   
   def makefile
@@ -29,8 +28,8 @@ module CapybaraWebkitBuilder
       system("#{qmake_bin} -spec linux-g++")
     when /freebsd/
       system("#{qmake_bin} -spec freebsd-g++")
-	when /mingw32/
-	  system("#{qmake_bin} -spec win32-g++")
+    when /mingw32/
+      system("#{qmake_bin} -spec win32-g++")
     else
       system("#{qmake_bin} -spec macx-g++")
     end
@@ -41,24 +40,24 @@ module CapybaraWebkitBuilder
   end
 
   def path_to_binary
-	case RUBY_PLATFORM
-	when /mingw32/
-	  'src/debug/webkit_server.exe'
-	else
-      "src/webkit_server"
-	end
+    case RUBY_PLATFORM
+    when /mingw32/
+      'src/debug/webkit_server.exe'
+    else
+      'src/webkit_server'
+    end
   end
   
   def build
     system(make_bin) or return false
 
     FileUtils.mkdir("bin") unless File.directory?("bin")	
-	FileUtils.cp(path_to_binary, "bin", :preserve => true)	
+    FileUtils.cp(path_to_binary, "bin", :preserve => true)	
   end
 
   def build_all
     makefile &&
-    qmake &&
-    build
+      qmake &&
+      build
   end
 end
