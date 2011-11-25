dc8e0eb6544d980a9bcdc2c26f57449a68ca9220
Fabrizio Regini <freegenie@gmail.com>
2011-11-26 00:10:59 +0100

    New option to ignore ssl errors

--- lib/capybara/webkit.rb
+++ lib/capybara/webkit.rb
@@ -5,6 +5,11 @@ Capybara.register_driver :webkit do |app|
   Capybara::Driver::Webkit.new(app)
 end
 
+Capybara.register_driver :webkit_ignore_ssl do |app|
+  browser = Capybara::Driver::Webkit::Browser.new(:ignore_ssl_errors => true)
+  Capybara::Driver::Webkit.new(app, :browser => browser)
+end
+
 Capybara.register_driver :webkit_debug do |app|
   browser = Capybara::Driver::Webkit::Browser.new(:socket_class => Capybara::Driver::Webkit::SocketDebugger)
   Capybara::Driver::Webkit.new(app, :browser => browser)
