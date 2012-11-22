c3eb16b87eb8641b71693e8d76c3e3d0ec010c7f
Matthew Horan <matt@matthoran.com>
2012-11-22 01:38:29 -0500

    Remove Qt 4.7 compatibility code from CurrentUrl

--- src/CurrentUrl.h
+++ src/CurrentUrl.h
@@ -6,11 +6,5 @@ class CurrentUrl : public SocketCommand {
   public:
     CurrentUrl(WebPageManager *, QStringList &arguments, QObject *parent = 0);
     virtual void start();
-#if QT_VERSION < QT_VERSION_CHECK(4, 8, 0)
-
-    private:
-      bool wasRegularLoad();
-      bool wasRedirectedAndNotModifiedByJavascript();
-#endif
 };
 
--- src/WebPage.cpp
+++ src/WebPage.cpp
@@ -34,7 +34,6 @@ WebPage::WebPage(WebPageManager *manager, QObject *parent) : QWebPage(parent) {
   resetWindowSize();
 
   settings()->setAttribute(QWebSettings::JavascriptCanOpenWindows, true);
-  currentFrame()->setUrl(QUrl("about:blank"));
 }
 
 void WebPage::resetWindowSize() {
