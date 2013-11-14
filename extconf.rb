dc328a2b9a0ad1ebe79a0d16d72106ebcbb9feea
Kashyap <kashyap.kmbc@gmail.com>
2013-11-14 14:15:13 +0530

    Throw an informative error if Qt is not present
    Right now, running "gem install capybara-webkit" on a machine that
    doesn't have Qt binaries installed, the installation will fail with the
    message "'qmake -spec macs-g++' not available". This change will check
    if a binary is installed and throw a descriptive error.

--- extconf.rb
+++ extconf.rb
@@ -1,2 +1,9 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), "lib", "capybara_webkit_builder")
-CapybaraWebkitBuilder.build_all
+
+if system("qmake").nil?
+  error = "Couldn't find the 'qmake' executable. Please install Qt and then proceed."
+
+  raise(error)
+else
+  CapybaraWebkitBuilder.build_all
+end
