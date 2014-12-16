f3bff4e8b8b46d9bb8ec0d93553862d60970af59
Charles Strahan <charles.c.strahan@gmail.com>
2014-12-15 19:50:53 -0500

    Expose INCLUDEPATH and LIBS qmake variables
    This allows one to specify the include and lib paths for gl and zlib.
    
    Example:
    
        gem install capybara-webkit -- \
          --with-gl-dir=/nix/store/1sw1cyny213ih9dpdsq8h2kwqaqcm6vp-mesa-10.2.9 \
          --with-zlib-dir=/nix/store/cb649pfdf14335d07jcfmsik7a1rsgbf-zlib-1.2.8
    
    Fixes #695

--- extconf.rb
+++ extconf.rb
@@ -1,2 +1,18 @@
+require 'mkmf'
+$CPPFLAGS = ""
+
+dir_config("gl")
+dir_config("zlib")
+
+includepath = $CPPFLAGS.gsub("-I", '').strip
+libs = $LIBPATH.map {|p| "-L#{p}"}.join(" ").strip
+
+unless includepath.empty?
+  ENV["CAPYBARA_WEBKIT_INCLUDEPATH"] = includepath
+end
+unless libs.empty?
+  ENV["CAPYBARA_WEBKIT_LIBS"] = libs
+end
+
 require File.join(File.expand_path(File.dirname(__FILE__)), "lib", "capybara_webkit_builder")
 CapybaraWebkitBuilder.build_all
